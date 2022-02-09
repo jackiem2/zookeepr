296f7c9e3ac186c607163c2631939dc783890ca3
jackiem2 <jacqueline.merlan@gmail.com>
2022-02-09 15:29:52 -0600

    added JSON app variable

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const { animals } = require('./data/animals')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   // Note that we save the animalsArray as filteredResults here:
@@ -58,6 +63,12 @@ app.get('/api/animals', (req, res) => {
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
       res.json(result);
+  });
+
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
   });
 
 app.listen(PORT, () => {
